b42d28ed41bd6f06ef3b429ac755791c8847ac27
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-04-02 13:12:29 -0300

    bluej - Dia01

---
+++ src/modulo-01-java-OO/Dia01/Elfo.java
@@ -0,0 +1,61 @@
+
+/**
+ * Define objetos Elfo
+ * 
+ * @author CWI Software
+ */
+public class Elfo
+{
+    private String nome;
+    private int flechas, experiencia;
+
+    // type initializer
+    {
+        flechas = 42;
+    }
+    
+    /**
+     * Cria objetos Elfo
+     * 
+     * @param umNome Nome que o elfo receberá
+     * @param flechas Quantidade inicial de flechas
+     */
+    public Elfo(String umNome, int flechas)
+    {
+        this(umNome);
+        // this.nome = nome;
+        this.flechas = flechas;
+    }
+    
+    public Elfo(String nome) {
+        this.nome = nome;
+    }
+    
+    /**
+     * Atira uma flecha e perde uma unidade.
+     */
+    public void atirarFlecha() {
+        //flechas = flechas - 1;
+        flechas--;
+        experiencia++;
+    }
+    
+    public void setNome(String novoNome) {
+        nome = novoNome;
+    }
+    
+    /*public void setFlechas(int novaQtdFlechas) {
+        if (novaQtdFlechas > flechas) {
+            flechas = novaQtdFlechas;
+        }
+    }*/
+    
+    // camelCase: public void atirarFlechaDourada
+    // PascalCase: public void AtirarFlechaDourada
+    
+    
+    
+    
+    
+    
+}
