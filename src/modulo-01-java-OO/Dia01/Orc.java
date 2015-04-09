f23cfba44d6c59472a276ee9c6afdfca51639aed
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-04-09 11:06:34 -0300

    Revert "feat(Orc) getItemComMaiorQuantidade()"
    This reverts commit f860fadfb90ec335f3bac6999565da9e4a07bd62.

--- src/modulo-01-java-OO/Dia01/Orc.java
+++ src/modulo-01-java-OO/Dia01/Orc.java
@@ -292,26 +292,9 @@ public class Orc
            }
        }
    }
-   
-   public String getItemComMaiorQuantidade(){
-       StringBuilder builder = new StringBuilder();         
-       int numeroItens = this.list.size();
-       int maior = 0;
-            for (int i = 0; i < numeroItens; i++){
-                ItemDoInventario itemAtual = this.list.get(i);
-                if (i == 0){
-                    maior = itemAtual.getQuantidade();
-                } else if(itemAtual.getQuantidade() > maior){
-                    maior = itemAtual.getQuantidade();
-                    builder.append(itemAtual.getDescricao());
-                } 
-                  
-            }
-            return builder.toString();
-   
-   }
-   
+
 }
+   
    
 
 
