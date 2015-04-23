5a64633274033f3f8e31c968285ba4a7c5e4fe8d
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-04-23 01:02:34 -0300

    feat(AulaSQL 4 - Exercicios 3 até 5)

--- src/modulo-02-banco-SQL/AulaSQL 4.sql
+++ src/modulo-02-banco-SQL/AulaSQL 4.sql
@@ -59,3 +59,49 @@ Inner Join Departamento on Departamento.IDDepartamento = Empregado.IDDepartament
 Select a.Nome, c.Nome
 From Associado a 
 Left Join Cidade c on a.IDCidade = c.IDCidade;
+
+/* Exercício 3 - Lista os estados (UF) e total de cidades que não possuem associados relacionados
+(dica: pode ser utilizado count + group by + exists).*/
+Select IDCidade, Nome 
+From Cidade e
+Where NOT EXISTS(Select IDCidade, Nome
+From Associado a
+Where a.IDCidade = e.IDCidade);
+
+/* Exercício 4 - Faça uma consulta que liste o nome do associado, o nome da cidade,
+ e uma coluna que indique se a cidade é da região SUL (RS, SC, PR),
+se for imprimir *** (3 asteriscos), senão imprimir nulo. */
+SELECT a.Nome, c.Nome,
+	Case
+	when c.UF = 'RS' or c.UF = 'SC' or c.UF = 'PR' then '***' 	 
+End UF
+FROM Associado a 
+INNER JOIN Cidade c on ISNULL (a.IDCidade,0) = ISNULL (c.IDCidade,0);
+--teste do UF ***
+Begin Transaction
+update Cidade set UF = 'SC' where IDCidade = 1; 
+update Cidade set UF = 'PR' where IDCidade = 1;  
+--teste do Associado NULL
+insert into Associado 
+      (IDAssociado, 
+       Nome, 
+       DataNascimento, 
+       Sexo,
+	   IDCidade)
+  values  
+      (3, 
+       'Rodrigo Molina', 
+       convert(datetime, '20/02/1980', 103), 
+       'M',
+	    106);
+ 
+rollback 
+
+/* Exercício 5 - Liste o nome do empregado, o nome do gerente, e o departamento de cada um. */
+Select e.NomeEmpregado as NomeEmpregado, de.NomeDepartamento as DepEmpregado, g.NomeEmpregado as NomeGerente, dg.NomeDepartamento as DepGerente
+From Empregado e
+INNER JOIN Empregado g ON e.IDGerente = g.IDEmpregado
+INNER JOIN Departamento de ON e.IDDepartamento = de.IDDepartamento
+INNER JOIN Departamento dg ON g.IDDepartamento = dg.IDDepartamento;
+-- Teste para verificar departamento dos empregados
+Select e.NomeEmpregado, d.NomeDepartamento from Empregado e, Departamento d where e.IDDepartamento = d.IDDepartamento; 
