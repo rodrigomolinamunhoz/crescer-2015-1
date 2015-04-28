c673ea97b1a21eb57c3c3f734d48c4a3b3c91307
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-04-28 01:34:33 -0300

    feat(Aula 8 - Exercícios Join 3 até 5)

--- src/modulo-02-banco-SQL/BancoCrescer/Crescer15-1.sql
+++ src/modulo-02-banco-SQL/BancoCrescer/Crescer15-1.sql
@@ -16,5 +16,27 @@ Situação: A */
 begin transaction
 insert into Produto(IDProduto,Nome,DataCadastro,PrecoCusto,PrecoVenda,Situacao)
 VALUES
-();
+('Galocha Maragato',GETDATE(),35.67,77.95,'A');
+--teste
+select * from Produto where Nome = 'Galocha Maragato'; 
+roolback
 
+/*4) Identifique e liste os produtos que não tiveram nenhum pedido,
+considere os relacionamentos no modelo de dados, pois não há relacionamento direto entre Produto e 
+Pedido (será preciso relacionar PedidoItem). Obs.: o produto criado anteriormente deverá ser listado.*/
+
+Select p.IDProduto, p.Nome
+from Produto p where not exists(select 1
+								from PedidoItem
+								where p.IDProduto = PedidoItem.IDProduto);
+
+
+
+/*5) Identifique qual o estado (coluna UF da tabela Cidade) possuí o maior número de clientes (tabela Cliente),
+liste também qual o Estado possuí o menor número de clientes.
+Dica: pode (não é obrigatório) ser utilizado subquery, e também UNION.*/
+
+Select cli.IDCliente from Cliente cli where  EXISTS (select count(1), c.UF
+										  from Cidade c
+										  where cli.IDCidade = c.IDCidade
+										  group by c.UF)
